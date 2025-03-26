6a7b1561b8725383d362639a7f133b7644e93a66
mukumuku <andreletourneau2@gmail.com>
2025-03-26 18:19:20 -0500

    implemented level up screen and dumped battle screen (but didn't install it yet)

---
+++ Graphics/Menu Tiles/TSA/tmx_cmp/Level Up Screen.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Level Up Screen" tilewidth="8" tileheight="8" tilecount="70" columns="14">
+ <image source="../../Level Up Screen.png" width="112" height="40"/>
+</tileset>
