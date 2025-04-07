00a4530dd1f183cf13beb82f2386a65172b23623
mukumuku <andreletourneau2@gmail.com>
2025-04-07 10:34:19 -0500

    armsthrift and added 2x crit

--- ASM/Formulas/Formulas.c
+++ ASM/Formulas/Formulas.c
@@ -11,6 +11,61 @@ void ComputeBattleUnitCritRate(struct BattleUnit* bu) {
         bu->battleCritRate += 15;
 }
 
-void ComputeBattleUnitDodgeRate(struct BattleUnit* bu) {
+void NewComputeBattleUnitDodgeRate(struct BattleUnit* bu) {
     bu->battleDodgeRate = 0;
+}
+
+void BattleGenerateHitAttributes(struct BattleUnit* attacker, struct BattleUnit* defender) {
+    short attack, defense;
+
+    gBattleStats.damage = 0;
+
+    BattleCheckSureShot(attacker);
+
+    if (!(gBattleHitIterator->attributes & BATTLE_HIT_ATTR_SURESHOT)) {
+        if (!BattleRoll2RN(gBattleStats.hitRate, TRUE)) {
+            gBattleHitIterator->attributes |= BATTLE_HIT_ATTR_MISS;
+            return;
+        }
+    }
+
+    attack = gBattleStats.attack;
+    defense = gBattleStats.defense;
+
+    BattleCheckGreatShield(attacker, defender);
+
+    if (!(gBattleHitIterator->attributes & BATTLE_HIT_ATTR_GREATSHLD))
+        BattleCheckPierce(attacker, defender);
+
+    if (gBattleHitIterator->attributes & BATTLE_HIT_ATTR_PIERCE)
+        defense = 0;
+
+    gBattleStats.damage = attack - defense;
+
+    if (gBattleHitIterator->attributes & BATTLE_HIT_ATTR_GREATSHLD)
+        gBattleStats.damage = 0;
+
+    if (BattleRoll1RN(gBattleStats.critRate, FALSE) == TRUE) {
+        if (BattleCheckSilencer(attacker, defender)) {
+            gBattleHitIterator->attributes |= BATTLE_HIT_ATTR_SILENCER;
+
+            gBattleStats.damage = BATTLE_MAX_DAMAGE;
+
+            gBattleHitIterator->attributes = gBattleHitIterator->attributes &~ BATTLE_HIT_ATTR_GREATSHLD;
+        } else {
+            gBattleHitIterator->attributes = gBattleHitIterator->attributes | BATTLE_HIT_ATTR_CRIT;
+            gBattleStats.damage = gBattleStats.damage * 3;
+        }
+    }
+
+    if (gBattleStats.damage > BATTLE_MAX_DAMAGE)
+        gBattleStats.damage = BATTLE_MAX_DAMAGE;
+
+    if (gBattleStats.damage < 0)
+        gBattleStats.damage = 0;
+
+    BattleCheckPetrify(attacker, defender);
+
+    if (gBattleStats.damage != 0)
+        attacker->nonZeroDamage = TRUE;
 }
